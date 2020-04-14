7736d5eecf9d5840b49fb8ce432dba3b682c1d4d
Shawn <shawnmann0203@gmail.com>
2020-04-14 16:42:18 -0700

    4/14/20 4:41pm Update

--- storeDB/src/entity/Customer.java
+++ storeDB/src/entity/Customer.java
@@ -6,6 +6,36 @@ public class Customer {
 	private String first;
 	private String last;
 	
+	public Customer(int customerId, String firstName, String lastName) {
+		this.setId(customerId);
+		this.setFirst(firstName);
+		this.setLast(lastName);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirst() {
+		return first;
+	}
+
+	public void setFirst(String first) {
+		this.first = first;
+	}
+
+	public String getLast() {
+		return last;
+	}
+
+	public void setLast(String last) {
+		this.last = last;
+	}
+	
 	
 	
 	
--- storeDB/src/entity/Product.java
+++ storeDB/src/entity/Product.java
@@ -1,5 +1,46 @@
 package entity;
 
 public class Product {
+	
+	private int id;
+	private String name;
+	private String type;
+	
+	
+	public Product(int id, String name, String type) {
+		this.setId(id);
+		this.setName(name);
+		this.setType(type);
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getType() {
+		return type;
+	}
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
 
 }
